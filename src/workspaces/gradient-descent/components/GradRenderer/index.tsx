61679f531b7202ab9efbde5b8a847be263c0d369
Rn86222 <rn86222.light0213@gmail.com>
2023-04-15 16:23:37 +0900

    import

--- src/workspaces/gradient-descent/components/GradRenderer/index.tsx
+++ src/workspaces/gradient-descent/components/GradRenderer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { objectiveFunction } from "../../objective";
