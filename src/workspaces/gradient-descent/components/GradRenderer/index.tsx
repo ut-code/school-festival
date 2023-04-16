165284127fccac7372c8e40012575d85d61c4f45
Rn86222 <rn86222.light0213@gmail.com>
2023-04-16 15:03:58 +0900

    create graph with triangles

--- src/workspaces/gradient-descent/components/GradRenderer/index.tsx
+++ src/workspaces/gradient-descent/components/GradRenderer/index.tsx
@@ -15,53 +15,79 @@ export function GradResetCamera() {
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 }
 
+function createAxis(max: number, direction: THREE.Vector3, color: number) {
+  const axisLength = max * 2;
+  const axisHeadLength = axisLength * 0.05;
+  const axisHeadWidth = axisHeadLength * 0.5;
+  const start = new THREE.Vector3(
+    -max * direction.x,
+    -max * direction.y,
+    -max * direction.z
+  );
+  const axis = new THREE.ArrowHelper(
+    direction,
+    start,
+    axisLength + axisHeadLength * 2,
+    color,
+    axisHeadLength,
+    axisHeadWidth
+  );
+  return axis;
+}
+
+function createMeshOfPoints(
+  color: number,
+  size: number,
+  points: number[],
+  transparent: boolean,
+  opacity: number
+) {
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute(
+    "position",
+    new THREE.Float32BufferAttribute(points, 3)
+  );
+  const material = new THREE.PointsMaterial({
+    size,
+    color,
+    transparent,
+    opacity,
+  });
+  const mesh = new THREE.Points(geometry, material);
+  return mesh;
+}
+
+function createTriangle(vertices: THREE.Vector3[], color: number) {
+  // ポリゴン面を構成する頂点のインデックス (Face3の代替となる情報)
+  const faces = [0, 1, 2, 0, 2, 3];
+
+  const geometry = new THREE.BufferGeometry();
+
+  // 頂点情報をBufferGeometryにセット
+  // BufferAttributeを生成する代わりに、setFromPointsを呼ぶと内部でいいようにやってくれる
+  geometry.setFromPoints(vertices);
+
+  // ポリゴン面を構成する頂点のインデックスをセット
+  geometry.setIndex(faces);
+
+  const material = new THREE.MeshBasicMaterial({
+    color,
+    side: THREE.DoubleSide,
+    opacity: 0.5,
+    transparent: true,
+  });
+
+  const triangleMesh = new THREE.Mesh(geometry, material);
+
+  return triangleMesh;
+}
+
 export function GradRenderer(props: {
   x: number;
   y: number;
   xAnswer: number;
   yAnswer: number;
 }) {
-  function createAxis(max: number, direction: THREE.Vector3, color: number) {
-    const axisLength = max * 2;
-    const axisHeadLength = axisLength * 0.05;
-    const axisHeadWidth = axisHeadLength * 0.5;
-    const start = new THREE.Vector3(
-      -max * direction.x,
-      -max * direction.y,
-      -max * direction.z
-    );
-    const axis = new THREE.ArrowHelper(
-      direction,
-      start,
-      axisLength + axisHeadLength * 2,
-      color,
-      axisHeadLength,
-      axisHeadWidth
-    );
-    return axis;
-  }
-  function createMeshOfPoints(
-    color: number,
-    size: number,
-    points: number[],
-    transparent: boolean,
-    opacity: number
-  ) {
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute(
-      "position",
-      new THREE.Float32BufferAttribute(points, 3)
-    );
-    const material = new THREE.PointsMaterial({
-      size,
-      color,
-      transparent,
-      opacity,
-    });
-    const mesh = new THREE.Points(geometry, material);
-    return mesh;
-  }
-
   const graph = () => {
     // レンダラを作成
     const renderer = new THREE.WebGLRenderer({
@@ -93,16 +119,38 @@ export function GradRenderer(props: {
       document.querySelector("#graph") as HTMLElement
     );
 
-    // 3次元グラフを点群で表示
-    const vertices = [];
-    for (let x = -1.5 * maxes[0]; x <= 1.5 * maxes[0]; x += 1) {
-      for (let z = -1.5 * maxes[2]; z <= 1.5 * maxes[2]; z += 1) {
-        const y = objectiveFunction(x, z, props.xAnswer, props.yAnswer);
-        vertices.push(x, y, z);
+    const interval = 15.0;
+    for (let x = -1.5 * maxes[0]; x <= 1.5 * maxes[0]; x += interval) {
+      for (let z = -1.5 * maxes[2]; z <= 1.5 * maxes[2]; z += interval) {
+        const vertices = [];
+        const y0 = objectiveFunction(x, z, props.xAnswer, props.yAnswer);
+        const y1 = objectiveFunction(
+          x + interval,
+          z,
+          props.xAnswer,
+          props.yAnswer
+        );
+        const y2 = objectiveFunction(
+          x + interval,
+          z + interval,
+          props.xAnswer,
+          props.yAnswer
+        );
+        const y3 = objectiveFunction(
+          x,
+          z + interval,
+          props.xAnswer,
+          props.yAnswer
+        );
+        vertices.push(new THREE.Vector3(x, y0, z));
+        vertices.push(new THREE.Vector3(x + interval, y1, z));
+        vertices.push(new THREE.Vector3(x + interval, y2, z + interval));
+        vertices.push(new THREE.Vector3(x, y3, z + interval));
+
+        const mesh = createTriangle(vertices, 0xffffff);
+        scene.add(mesh);
       }
     }
-    const meshOfGraph = createMeshOfPoints(0xffffff, 5, vertices, true, 0.2);
-    scene.add(meshOfGraph);
 
     // 現在の位置を表示
     const point = [
