9c292c41c7831cf77f239b7be1632dfc45b93a28
taka2 <taka23131@gmail.com>
2023-04-16 12:20:58 +0900

    迷路を追加

--- src/workspaces/qlearning/index.tsx
+++ src/workspaces/qlearning/index.tsx
@@ -1,10 +1,7 @@
 import { useRef, useState } from "react";
-import { Box, Grid, Text } from "@chakra-ui/react";
+import { Box, Grid } from "@chakra-ui/react";
 import { useGetSet } from "react-use";
-import {
-  BlocklyEditorMessage,
-  useBlocklyInterpreter,
-} from "../../commons/interpreter";
+import { useBlocklyInterpreter } from "../../commons/interpreter";
 import {
   BlocklyToolboxDefinition,
   useBlocklyWorkspace,
@@ -35,6 +32,8 @@ import {
   CUSTOM_COMMON_IF,
   CUSTOM_COMMON_DO_UNTIL,
 } from "../../config/blockly.blocks";
+import { MazeRenderer } from "./MazeRenderer";
+import { Maze, createMaze } from "../../commons/maze";
 
 const toolboxDefinition: BlocklyToolboxDefinition = {
   type: "category",
@@ -73,21 +72,33 @@ const toolboxDefinition: BlocklyToolboxDefinition = {
   enableVariables: true,
 };
 
+type MazeWorkspaceState = {
+  maze: Maze;
+  location: { x: number; y: number };
+};
+
+function createDefaultState(): MazeWorkspaceState {
+  return {
+    maze: createMaze(10, 10),
+    location: { x: 0, y: 0 },
+  };
+}
+
 export function QlearningWorkspace(): JSX.Element {
   // interpreter に渡す関数は実行開始時に決定されるため、通常の state だと最新の情報が参照できません
   // このため、反則ですが内部的に ref を用いて状態管理をしている react-use の [useGetSet](https://github.com/streamich/react-use/blob/master/docs/useGetSet.md) を用いています。
-  const [getState, setState] = useGetSet(0);
+  const [getState, setState] = useGetSet(createDefaultState());
 
   // javascriptGenerator により生成されたコードから呼ばれる関数を定義します
   const globalFunctions = useRef({
-    [CUSTOM_TEMPLATE_INCREMENT]: (value: number) => {
-      const currentState = getState();
-      const newState = currentState + value;
-      setState(newState);
-      // GlobalFunction 内で BlocklyEditorMessage オブジェクトをスローすると「情報」スナックバーが表示され、実行が停止されます
-      if (newState >= 10) throw new BlocklyEditorMessage("10 になりました！");
-      // GlobalFunction 内で Error オブジェクトをスローすると「エラー」スナックバーが表示され、実行が停止されます
-      if (newState < 0) throw new Error("残念！ゼロを下回ってしまいました...");
+    [CUSTOM_TEMPLATE_INCREMENT]: () => {
+      // const currentState = getState();
+      // const newState = currentState + value;
+      // setState(newState);
+      // // GlobalFunction 内で BlocklyEditorMessage オブジェクトをスローすると「情報」スナックバーが表示され、実行が停止されます
+      // if (newState >= 10) throw new BlocklyEditorMessage("10 になりました！");
+      // // GlobalFunction 内で Error オブジェクトをスローすると「エラー」スナックバーが表示され、実行が停止されます
+      // if (newState < 0) throw new Error("残念！ゼロを下回ってしまいました...");
     },
   }).current;
 
@@ -113,10 +124,10 @@ export function QlearningWorkspace(): JSX.Element {
             interpreter.start(getCode());
           }}
           onReset={() => {
-            setState(0);
+            setState({ ...getState(), location: { x: 0, y: 0 } });
           }}
         />
-        <Text mt={2}>{getState()}</Text>
+        <MazeRenderer maze={getState().maze} location={getState().location} />
       </Box>
     </Grid>
   );
